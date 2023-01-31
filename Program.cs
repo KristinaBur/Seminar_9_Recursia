97b3e1841106f7be183c8dd0247436ec2a832edb
«Kristina» <kristinka007_92@mail.ru>
2023-01-31 14:02:00 +0300

    Задача 66

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
 
-Console.Write("Введите натуральное число: ");
+/*Console.Write("Введите натуральное число: ");
 int N = int.Parse(Console.ReadLine()!);
             
             void NumberCounter(int N)
@@ -14,4 +14,30 @@ int N = int.Parse(Console.ReadLine()!);
                 NumberCounter(N - 1);
             }
             
-            NumberCounter(N); 
+            NumberCounter(N); */
+
+
+/*Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке
+ от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30  */
+
+Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine()!);
+
+void NumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    NumberSum(numberM, numberN, sum);
+}
+
+NumberSum(numberM, numberN, 0);
